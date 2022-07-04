9360377f443403e67001c2190551eb7e180589a5
ankit <ankit93598321@gmail.com>
2022-07-04 19:35:30 +0530

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Made with ♥ by{" "}
+        <a
+          href="https://www.youtube.com/roadsidecoder"
+          style={{ cursor: "pointer" }}
+        >
+          Ankit Mittal
+        </a>
+      </div>
+    );
+  };
+  
+  export default Footer;
+  
